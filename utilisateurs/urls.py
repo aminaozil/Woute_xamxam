99ba7573047b2b9d21009b33db3750cbf5e4d802
aminaozil <youmaminatambengue@gmail.com>
2024-12-13 21:06:56 +0000

    connexion

--- utilisateurs/urls.py
+++ utilisateurs/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import login_form, register_form
+from .views import login_form, register_form, logout_view
 
 urlpatterns = [
-    path("login/", login_form, name="login"),
-    path("inscription/", register_form, name="inscription")
+    path("connexion/", login_form, name="connexion"),
+    path("inscription/", register_form, name="inscription"),
+    path("deconnexion/", logout_view, name="deconnexion"),
+
 ]
--- utilisateurs/views.py
+++ utilisateurs/views.py
@@ -1,21 +1,33 @@
+from django.contrib import messages
 from django.shortcuts import redirect, render
 from .form import CustomUserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+
 
 
 def login_form(request):
-   
-   
-    return render(request, "utilisateurs/login.html", {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/list_articles/")
+        else: 
+            messages.error(request, 'Nom d\'utilisateur ou mot de passe incorrect.')
+    return render(request, "utilisateurs/login.html")
 
 def register_form(request):
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("/list_articles/")
+            return redirect("/connexion/")
     else:
         form = CustomUserCreationForm()
     return render(request, "utilisateurs/register.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("/connexion/")
