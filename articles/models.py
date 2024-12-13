8a8c7c1b71d077f1cd349e1182781678f8f000c0
aminaozil <youmaminatambengue@gmail.com>
2024-12-13 23:27:29 +0000

    model article

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+
+class Article(models.Model):
+    categorie = models.CharField(max_length=255)
+    titre = models.CharField(max_length=255)
+    img = models.ImageField(upload_to='images/')
+    resume = models.CharField(max_length=255)
+    contenu = models.TextField()
+    date_creation = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+    auteur = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
